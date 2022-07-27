4d35097c874209256160ec0ee373352d8828a219
Subhankar <ssubhankar2007@gmail.com>
2022-07-28 01:18:52 +0530

    Create EssentialFeed framework

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Subhankar Ghosh on 28/07/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
